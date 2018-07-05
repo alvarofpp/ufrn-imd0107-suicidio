090bc861f9c5b9fb3001950aa973dc570c8cffb9
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-07-04 23:18:21 -0300

    Site

---
+++ site/js/custom.js
@@ -0,0 +1,9 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+  });
+
+})(jQuery); // End of use strict
