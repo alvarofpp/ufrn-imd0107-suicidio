3685044c82a0d520febb92739d058a248a5b3bab
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-07-05 00:59:43 -0300

    Primeiro gráfico no site

--- site/js/custom.js
+++ site/js/custom.js
@@ -1,9 +1,44 @@
-(function($) {
-  "use strict"; // Start of use strict
+var jsonFile = '/home/roleta/anaconda-projects/imd0107_suicidio/site/data/racacor_estciv_sexo.json';
+console.log(jsonFile);
 
-  // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
-    e.preventDefault();
+$(function () {
+  $.getJSON(jsonFile, function (dataJson) {
+    // Estados civis
+    var estadosCivis = ['Solteiro(a)', 'Casado(a)', 'Divorciado(a)', 'Viúvo(a)', 'Separado'];
+
+    // Sexo
+    var sexo = ['Masculino', 'Feminino'];
+
+    var chart = Highcharts.chart('racacor_estciv_sexo', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Suicídios'
+      },
+      xAxis: {
+        categories: estadosCivis
+      },
+      yAxis: [{
+        min: 0,
+        title: {
+          text: 'Quantidade'
+        }
+      }],
+      legend: {
+        shadow: false
+      },
+      tooltip: {
+        shared: true
+      },
+      plotOptions: {
+        column: {
+          grouping: false,
+          shadow: false,
+          borderWidth: 0
+        }
+      },
+      series: dataJson
+    });
   });
-
-})(jQuery); // End of use strict
+});
